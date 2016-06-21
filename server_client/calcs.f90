a11dd1861772ccc739871b147a71a1aaf233934b
simstra <simstra@student.chalmers.se>
2016-06-21 15:03:11 -0400

    Added comments and general clean up
    Summary says it all

--- server_client/calcs.f90
+++ server_client/calcs.f90
@@ -1,4 +1,13 @@
-program name
+program calculator_server
+! 
+! Authors:
+!   Lina Olandersson, Simon Strandberg, Erik Thorsell
+! Date:
+!   2016-06-21
+!
+! Calculator at the server side of the client/server calculator
+! Responsible for starting up the server server.c
+
 use iso_c_binding, only: C_CHAR, C_NULL_CHAR, C_INT
 implicit none
 
@@ -6,6 +15,7 @@ implicit none
     character(255) query
     integer calc, ans, portnum, calculate
 
+    ! Interface that ensures type compatibility between Fortran and C
     interface
         subroutine server(portnum) bind(C, name="server")
             use iso_c_binding, only: c_int
@@ -13,16 +23,28 @@ implicit none
         end subroutine server
     end interface
 
-    ! executable statements
+    ! Start the server with portnumber read from standard in
     print *, "Please provide me with a port number."
     read "(1i9)", portnum
     call server(portnum)
 
-end program name
+end program calculator_server
+
+
+! **********************************************************************
 
 integer(kind=c_int) function calculate(query) bind (C, name="calculate")
+! Authors:
+!   Lina Olandersson, Simon Strandberg, Erik Thorsell
+! Date:
+!   2016-06-21
+!
+! Calculator function. Takes two numbers, an operator and does the calculation
+
 use iso_c_binding, only: C_CHAR, C_NULL_CHAR, C_INT
     implicit none
+
+    ! Variable declarations
     character(kind = C_CHAR, len=1), dimension(255) :: query
     integer length, i, nargs, len, arraysize,k,tmp
     character(255) operator
@@ -31,7 +53,11 @@ use iso_c_binding, only: C_CHAR, C_NULL_CHAR, C_INT
     len = 0
     calculate = 0
 
+    ! Call the parse subroutine to parse the string and put it in an array
     call parse(query, inputarray, nargs)
+
+    ! Check which operator was sent (last part of the input string)
+    ! Then choose accordingly and calculate
 
     operator = inputarray(nargs)
 
@@ -64,13 +90,24 @@ use iso_c_binding, only: C_CHAR, C_NULL_CHAR, C_INT
         calculate = calculate/tmp
     end if
 
-   ! print *, "Calculate:", calculate
 
 end function calculate
 
+
+! ****************************************************************
+
 subroutine parse(indata, inputarray, nargs)
+!
+! Author:
+!   Simon Strandberg
+! Date:
+!   2016-06-21
+!
+! Subroutine to parse a string
+
 implicit none
 
+! Variable declarations
     character(255) :: indata, string1, string2, delim
     character(255), dimension (255) :: inputarray
     integer :: count, length, nargs, pos, i
@@ -79,10 +116,12 @@ implicit none
     pos = 1
     delim = ' '
 
+! Check actual length of indata. Not interested in blanks at the end
     length = len_trim(indata)
 
     indata = (indata(1:length))
 
+! Scan the string for blankspaces to check total number of arguments. 
     do i=1,length
         pos = SCAN(indata(i:length), delim)
         if (pos==1) then
@@ -90,8 +129,8 @@ implicit none
         end if
     end do
 
+! Split the indata into an array. All data is still formated as type character.
     do i=1,nargs
-
         if (SCAN(indata,' ')/=0) then
             call split_string(indata, string1, string2, delim)
             inputarray(count) = string1
@@ -100,9 +139,17 @@ implicit none
             count=count + 1
         end if
     end do
+
 end subroutine parse
 
+
+! ******************************************************************
+
 SUBROUTINE split_string(instring, string1, string2, delim)
+!
+! Split function made by ponderomotion
+! URL: https://gist.github.com/ponderomotion/3833266
+
 implicit none
     CHARACTER(255) :: instring,delim
     CHARACTER(255),INTENT(OUT):: string1,string2
