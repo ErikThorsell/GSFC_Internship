f7f9886a5455289557a5065e606a0c580a61d80d
Erik Thorsell <erithor@student.chalmers.se>
2016-06-24 10:23:23 -0400

    Changed dimensions of master_io array.

--- performance_test/master_io.f90
+++ performance_test/master_io.f90
@@ -1,31 +1,31 @@
 program master
 ! Master writes a 10 000 bytes matrix to a file, then slave
 ! squares all numbers and writes it back to the file.
-! Author: Lina Olandersson	
+! Author: Lina Olandersson  
 ! Date: 2016-06-22
 implicit none
 
-integer*2, dimension (100,50) :: matrix     
-integer :: i, j
+integer*4, dimension (2500) :: matrix     
+integer :: length, i
 
 ! put integers in matrix and output data into a file 
-open(1, file='matrixdata.dat', status='new')  
-do i=1,100
-	do j = 1, 50
-	 	matrix(i, j) = i+j
-		write(1,*) matrix(i, j) 
-    end do   
-end do     
-close(1) 
+open(1, file='matrixdata.dat', status='new')
+
+length = 2500
+
+do i=1,length
+    matrix(i) = i
+    write(1,*) matrix(i)
+end do
+
+close(1)
 
 call slave
 
 ! open and read the file changed by subroutine slave
 open(1, file= 'matrixdata.dat', status='old')
 do i = 1, 100
-	do j = 1, 50
-		read(1,*) matrix(i,j)
-	end do
+    read(1,*) matrix(i)
 end do
 close(1)
 
@@ -35,26 +35,24 @@ end program master
 subroutine slave
 implicit none
 
-integer*2, dimension (100,50) :: matrix
-integer :: i,j
+integer*4, dimension (2500) :: matrix
+integer :: length, i
 
 ! Open and read the file made by master into a matrix
 open (1, file= 'matrixdata.dat', status = 'old')
-do i = 1, 100
-	do j = 1, 50
-		read(1,*) matrix(i,j)
-	end do
+length = 2500
+
+do i = 1, length
+    read(1,*) matrix(i)
 end do
 close(1)
 
 ! Square all numbers and write over the file with new data
 open(1, file= 'matrixdata.dat', status = 'old')
-do i=1,100
-	do j = 1, 50
-		matrix(i,j) = matrix(i,j)**2
-		write(1,*) matrix(i, j) 	
-   end do   
-end do     
+do i=1,length
+    matrix(i) = matrix(i)**2
+    write(1,*) matrix(i)     
+end do 
 close(1) 
 
 end subroutine slave
