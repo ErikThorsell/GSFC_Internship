28d419fe6439737f526d2b8a6c9f20228f83e58e
Erik Thorsell <erithor@student.chalmers.se>
2016-07-07 08:04:49 -0400

    Incremental push.

---
+++ parent_child/hwclient.f
@@ -0,0 +1,36 @@
+      program server
+        implicit none
+        include 'f77_zmq.h'
+        integer(ZMQ_PTR)        context
+        integer(ZMQ_PTR)        requester
+        character*(64)          address
+        character*(20)          buffer
+        integer                 rc
+        integer                 i
+       
+       
+        address = 'tcp://localhost:55555'
+       
+        context   = f77_zmq_ctx_new()
+        requester = f77_zmq_socket(context, ZMQ_REQ)
+        rc        = f77_zmq_connect(requester,address)
+       
+        print *,  rc
+        do i=1,10
+          print *,  "rc = f77_zmq_send(requester"
+          rc = f77_zmq_send(requester, "Hello!", 6, 0)
+          print *,  rc
+          print *,  "rc = f77_zmq_recv(requester, buffer, 20, 0)"
+          rc = f77_zmq_recv(requester, buffer, 20, 0)
+          print *,  rc
+          print *,  i, 'Received :', buffer(1:rc)
+        enddo
+        rc = f77_zmq_send(requester, "end", 3, 0)
+        rc = f77_zmq_recv(requester, buffer, 20, 0)
+
+        rc = f77_zmq_close(requester)
+        rc = f77_zmq_ctx_destroy(context)
+
+      end
+
+
