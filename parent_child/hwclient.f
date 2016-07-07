121b0465f3d10dbe7018f16d2eb53afde5b1c547
Erik Thorsell <erithor@student.chalmers.se>
2016-07-07 09:59:18 -0400

    First draft of parent/child.

--- parent_child/hwclient.f
+++ parent_child/hwclient.f
@@ -4,33 +4,83 @@
         integer(ZMQ_PTR)        context
         integer(ZMQ_PTR)        requester
         character*(64)          address
-        character*(20)          buffer
+        integer, dimension(20) :: buffer
         integer                 rc
-        integer                 i
-       
-       
+        integer                 i, lbuf, c, sbuf
+      
+      
         address = 'tcp://localhost:55555'
-       
+      
         context   = f77_zmq_ctx_new()
         requester = f77_zmq_socket(context, ZMQ_REQ)
         rc        = f77_zmq_connect(requester,address)
-       
-        print *,  rc
-        do i=1,10
-          print *,  "rc = f77_zmq_send(requester"
-          rc = f77_zmq_send(requester, "Hello!", 6, 0)
-          print *,  rc
-          print *,  "rc = f77_zmq_recv(requester, buffer, 20, 0)"
-          rc = f77_zmq_recv(requester, buffer, 20, 0)
-          print *,  rc
-          print *,  i, 'Received :', buffer(1:rc)
-        enddo
-        rc = f77_zmq_send(requester, "end", 3, 0)
-        rc = f77_zmq_recv(requester, buffer, 20, 0)
+      
+        lbuf = size(buffer)
+        sbuf = lbuf * 4
 
+        print *, "Size of buffer is: ", sbuf
+      
+        do i=1, lbuf
+          buffer(i) = 0
+        enddo
+      
+        ! Child is born and acknoweldges its existens to its parent.
+        print *,  "Awaken! Sending ack to Parent! RC: ", rc
+        rc = f77_zmq_send(requester, buffer, sbuf, 0) ! 1
+        print *,  "Sent ack, RC: ", rc
+
+        ! Due to ZeroMQ's implementation you need to send/recv 
+        rc = f77_zmq_recv(requester, buffer, sbuf, 0) ! 3
+        print *,  "3: Received dummy. RC: ", rc
+        if ((buffer(1) .eq. 9) .and. (rc .ge. 0)) then
+          print *, "All is good, please chose between the options below"
+          print *, "Choose what you want to do:"
+          print *, "(1) Send"
+          print *, "(2) Get"
+          print *, "(3) Send Obs"
+          print *, "(4) Get Obs"
+          print *, "(5) Get Size"
+          print *, "(6) Done"
+          print *, "(7) Exit"
+          read(*,*) c
+        
+          select case (c)
+            case (1)
+              print *, "Ask Parent to send array."
+              print *, "Which array do you want (1-3)?"
+              read(*,*) c
+              buffer(1) = 1
+              buffer(2) = c
+              rc = f77_zmq_send(requester, buffer, sbuf, 0) ! 5
+              print *, "Waiting for Parent to send array. RC: ", rc
+            case (2)
+            case (3)
+            case (4)
+            case (5)
+            case (6)
+            case (7)
+          end select
+        
+          !print *,  "rc = f77_zmq_recv(requester, buffer, 20, 0)"
+          if (rc .ge. 0) then
+            rc = f77_zmq_recv(requester, buffer, sbuf, 0)
+            print *, "Parent sent back the data. RC: ", rc
+            do i=1,lbuf
+                print *, buffer(i)
+            enddo
+          else
+            print *, "Something went wrong. RC: ", rc
+          endif
+        else
+            print *, "Something went wrong. RC: ", rc
+        endif
+      
+        !rc = f77_zmq_send(requester, "end", 3, 0)
+        !rc = f77_zmq_recv(requester, buffer, 20, 0)
+      
         rc = f77_zmq_close(requester)
         rc = f77_zmq_ctx_destroy(context)
-
+      
       end
-
-
+ 
+ 
--- parent_child/hwserver.f
+++ parent_child/hwserver.f
@@ -4,33 +4,68 @@
         integer(ZMQ_PTR)        context
         integer(ZMQ_PTR)        responder
         character*(64)          address
-        character(len=:), allocatable :: buffer
-        integer                 rc
-       
-       
-        allocate (character(len=20) :: buffer)
+        !integer, dimension(:), allocatable :: buffer
+        integer, dimension(20) :: buffer
+        integer, dimension(20) :: array1, array2, array3
+        integer                 rc, sbuf, i, lbuf
+
+
+        !allocate(buffer(3))
+        lbuf = size(buffer)
+        sbuf = lbuf*4
+        do i=1,lbuf
+            array1(i) = 1
+            array2(i) = 2
+            array3(i) = 3
+        enddo
+
         address = 'tcp://*:55555'
-       
+
         context   = f77_zmq_ctx_new()
         responder = f77_zmq_socket(context, ZMQ_REP)
         rc        = f77_zmq_bind(responder,address)
-       
-        print *,  rc
-        do
-          print *,  "rc = f77_zmq_recv(responder, buffer, 20, 0)"
-          rc = f77_zmq_recv(responder, buffer, 20, 0)
-          print *,  'Received :', buffer(1:rc)
-          if (buffer(1:rc) /= 'end') then
-            rc = f77_zmq_send (responder, "world", 5, 0)
-          else
-            rc = f77_zmq_send (responder, "end", 3, 0)
-            exit
-          endif
-        enddo
+
+        print *,  "Waiting for Child to acknowledge existence. RC: ", rc
+        rc = f77_zmq_recv(responder, buffer, sbuf, 0) ! 2
+        print *,  'Received ack from Child. RC: ', rc
+
+        ! buffer(1) = 9 will be used as an ack throughout the program.
+        ! ZeroMQ does not allow consecuetive sends/recvs, why each send has to
+        ! be followed by a recv as seen below.
+        buffer(1) = 9
+        rc = f77_zmq_send(responder, buffer, sbuf, 0) ! 4
+        print *,  'Waiting for Child. RC: ', rc
+        rc = f77_zmq_recv(responder, buffer, sbuf, 0) ! 6
+        print *, "Received: ", (buffer(1:3))
+
+        select case (buffer(1))
+          case (1)
+            print *, "Child asked for Parent to send array: ", buffer(2)
+            select case (buffer(2))
+              case (1)
+                  buffer = array1
+              case (2)
+                  buffer = array2
+              case (3)
+                  buffer = array3
+            end select
+            rc = f77_zmq_send(responder, buffer, sbuf, 0)
+            print *, "Sending array. RC: ", rc
+        end select
+
+
+        !if (buffer(1:rc) /= 'end') then
+        !  print *, "Child asked for: ",
+        !  rc = f77_zmq_send (responder, "world", 5, 0)
+        !else
+        !  rc = f77_zmq_send (responder, "end", 3, 0)
+        !  exit
+        !endif
+      !enddo
 
         rc = f77_zmq_close(responder)
         rc = f77_zmq_ctx_destroy(context)
-        deallocate(buffer)
+        !deallocate(buffer)
       end
 
 
