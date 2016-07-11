69f9f410d4ade977fe7372e225f8c7b37a5be3d0
Erik Thorsell <erithor@student.chalmers.se>
2016-07-11 15:12:32 -0400

    Renamed variables for clarity.

--- parent_child/hwserver.f
+++ parent_child/hwserver.f
@@ -6,100 +6,100 @@
         integer(ZMQ_PTR)        context
         integer(ZMQ_PTR)        responder
         character*(64)          address
-        integer, dimension(22) :: buffer
-        integer, dimension(20) :: array1, array2, array3
-        integer                 rc, sbuf, i, lbuf, c, size_commonblock
+        integer, dimension(22) :: i_buffer
+        integer, dimension(20) :: i_array1, i_array2, i_array3
+        integer                 i_rc, i_sbuf, i, i_lbuf, c, i_blocksize
 
 
-        !allocate(buffer(3))
-        lbuf = size(buffer)
-        sbuf = lbuf*4
-        do i=1,(lbuf-2)
-            array1(i) = 11
-            array2(i) = 12
-            array3(i) = 13
+        !allocate(i_buffer(3))
+        i_lbuf = size(i_buffer)
+        i_sbuf = i_lbuf*4
+        do i=1,(i_lbuf-2)
+            i_array1(i) = 11
+            i_array2(i) = 12
+            i_array3(i) = 13
         enddo
 
         address = 'tcp://*:55555'
 
         context   = f77_zmq_ctx_new()
         responder = f77_zmq_socket(context, ZMQ_REP)
-        rc        = f77_zmq_bind(responder,address)
+        i_rc        = f77_zmq_bind(responder,address)
 
         !call execute_command_line("./hwclient", wait=.false.)
 
         ! Find size of commonblock, add 2 for size of ILAST_OBORG_I2
-        size_commonblock = loc(ILAST_OBORG_I2)-loc(FJD) + 2
+        i_blocksize = loc(ILAST_OBORG_I2)-loc(FJD) + 2
 
         print *, OBORG_IFILL_LEN
-        print *,  "Waiting for Child to ack its existence. RC: ", rc
-        rc = f77_zmq_recv(responder, buffer, sbuf, 0) ! 2
-        print *,  'Received ack from Child. RC: ', rc
+        print *,  "Waiting for Child to ack its existence. i_rc: ", i_rc
+        i_rc = f77_zmq_recv(responder, i_buffer, i_sbuf, 0) ! 2
+        print *,  'Received ack from Child. i_rc: ', i_rc
 
-        ! buffer(1) = 9 will be used as an ack throughout the program.
+        ! i_buffer(1) = 9 will be used as an ack throughout the program.
         ! ZeroMQ does not allow consecuetive sends/recvs, why each send has to
         ! be followed by a recv as seen below.
-        buffer(1) = 9
-        rc = f77_zmq_send(responder, buffer, sbuf, 0) ! 4
+        i_buffer(1) = 9
+        i_rc = f77_zmq_send(responder, i_buffer, i_sbuf, 0) ! 4
 
       do while(.true.)
-        print *,  'Waiting for Child to perform operation. RC: ', rc
-        rc = f77_zmq_recv(responder, buffer, sbuf, 0) ! 6
-        print *, "Received operation code: ", (buffer(1:2))
+        print *,  'Waiting for Child to perform operation. i_rc: ', i_rc
+        i_rc = f77_zmq_recv(responder, i_buffer, i_sbuf, 0) ! 6
+        print *, "Received operation code: ", (i_buffer(1:2))
 
-        select case (buffer(1))
+        select case (i_buffer(1))
           case (1)
-            print *, "Child asked for Parent to send array: ", buffer(2)
-            c = buffer(2)
-            buffer(1) = 9
-            buffer(2) = 9
+            print *, "Child asked for Parent to send i_array: ", i_buffer(2)
+            c = i_buffer(2)
+            i_buffer(1) = 9
+            i_buffer(2) = 9
             select case (c)
               case (1)
-                  buffer(3:lbuf) = array1
+                  i_buffer(3:i_lbuf) = i_array1
               case (2)
-                  buffer(3:lbuf) = array2
+                  i_buffer(3:i_lbuf) = i_array2
               case (3)
-                  buffer(3:lbuf) = array3
+                  i_buffer(3:i_lbuf) = i_array3
             end select
-            rc = f77_zmq_send(responder, buffer, sbuf, 0)
-            print *, "Sending array. RC: ", rc
+            i_rc = f77_zmq_send(responder, i_buffer, i_sbuf, 0)
+            print *, "Sending i_array. i_rc: ", i_rc
 
           case (2)
-            print *, "Child asked Parent to get array", buffer(2)
-            c = buffer(2)
-            buffer(1) = 9
-            rc = f77_zmq_send(responder, buffer, sbuf, 0)
-            print *, "Parent acked that data can be sent. RC: ", rc
-            rc = f77_zmq_recv(responder, buffer, sbuf, 0)
-            !do i=3,(lbuf)
-            !  print *, buffer(i)
+            print *, "Child asked Parent to get i_array", i_buffer(2)
+            c = i_buffer(2)
+            i_buffer(1) = 9
+            i_rc = f77_zmq_send(responder, i_buffer, i_sbuf, 0)
+            print *, "Parent acked that data can be sent. i_rc: ", i_rc
+            i_rc = f77_zmq_recv(responder, i_buffer, i_sbuf, 0)
+            !do i=3,(i_lbuf)
+            !  print *, i_buffer(i)
             !enddo
             select case (c)
               case (1)
-                array1 = buffer(3:lbuf)
-                do i=1,(lbuf-2)
-                  print *, array1(i)
+                i_array1 = i_buffer(3:i_lbuf)
+                do i=1,(i_lbuf-2)
+                  print *, i_array1(i)
                 enddo
               case (2)
-                array2 = buffer(3:lbuf)
-                do i=1,(lbuf-2)
-                  print *, array2(i)
+                i_array2 = i_buffer(3:i_lbuf)
+                do i=1,(i_lbuf-2)
+                  print *, i_array2(i)
                 enddo
               case (3)
-                array3 = buffer(3:lbuf)
-                do i=1,(lbuf-2)
-                  print *, array3(i)
+                i_array3 = i_buffer(3:i_lbuf)
+                do i=1,(i_lbuf-2)
+                  print *, i_array3(i)
                 enddo
             end select
-            buffer(1) = 9
-            rc = f77_zmq_send(responder, buffer, sbuf, 0)
+            i_buffer(1) = 9
+            i_rc = f77_zmq_send(responder, i_buffer, i_sbuf, 0)
             print *, "Parent acked that it got the data!"
 
           case(3)
             print *, "Child asked parent to send commonblock for obs:"
-            print *, buffer(2)
-            c=buffer(2)
-            buffer(1) = 9
+            print *, i_buffer(2)
+            c=i_buffer(2)
+            i_buffer(1) = 9
 
             ! Assign values to forst and last entries for sanity check
             FJD = 1
@@ -109,49 +109,49 @@
             print *, "ILAST_OBORG_I2: ", ILAST_OBORG_I2
 
             ! Send commonblock to child
-            rc = f77_zmq_send(responder, FJD, size_commonblock, 0)
+            i_rc = f77_zmq_send(responder, FJD, i_blocksize, 0)
 
           case(4)
             print *, "Child wants to send common block for obs:"
-            print *, buffer(2)
-            c=buffer(2)
-            buffer(1) = 9
+            print *, i_buffer(2)
+            c=i_buffer(2)
+            i_buffer(1) = 9
 
             ! Acknomledgement, ready to receive again
-            rc = f77_zmq_send(responder, buffer, sbuf, 0)
+            i_rc = f77_zmq_send(responder, i_buffer, i_sbuf, 0)
 
             ! Receive commonblock
-            rc = f77_zmq_recv(responder, FJD, size_commonblock, 0)
+            i_rc = f77_zmq_recv(responder, FJD, i_blocksize, 0)
 
             print *, "FJD: ", FJD
             print *, "ILAST_OBORG_I2: ", ILAST_OBORG_I2
 
             ! Acknowledge that transfer is done.
-            buffer(1) = 9
-            rc = f77_zmq_send(responder, buffer, sbuf, 0)
+            i_buffer(1) = 9
+            i_rc = f77_zmq_send(responder, i_buffer, i_sbuf, 0)
 
           case(5)
           case(6)
           case(7)
-            buffer(1) = 9
+            i_buffer(1) = 9
             print *, "Child is done with its operations."
-            rc = f77_zmq_send(responder, buffer, sbuf, 0 )
+            i_rc = f77_zmq_send(responder, i_buffer, i_sbuf, 0 )
             exit
         end select
       enddo
 
-        !if (buffer(1:rc) /= 'end') then
+        !if (i_buffer(1:i_rc) /= 'end') then
         !  print *, "Child asked for: ",
-        !  rc = f77_zmq_send (responder, "world", 5, 0)
+        !  i_rc = f77_zmq_send (responder, "world", 5, 0)
         !else
-        !  rc = f77_zmq_send (responder, "end", 3, 0)
+        !  i_rc = f77_zmq_send (responder, "end", 3, 0)
         !  exit
         !endif
       !enddo
 
-        rc = f77_zmq_close(responder)
-        rc = f77_zmq_ctx_destroy(context)
-        !deallocate(buffer)
+        i_rc = f77_zmq_close(responder)
+        i_rc = f77_zmq_ctx_destroy(context)
+        !deallocate(i_buffer)
       end
 
 
