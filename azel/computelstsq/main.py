0f37d0c992bddc7fec2d8701d37afb5ed732a247
Lina Olandersson <olalina@student.chalmers.se>
2016-08-02 09:46:04 -0400

    Added plot and least square

--- azel/computelstsq/main.py
+++ azel/computelstsq/main.py
@@ -10,28 +10,28 @@ import csv
 
 # Get data from files
 
-with open('az_slew') as csvfile:
+with open('../script/ft_az.dat') as csvfile:
     readCSV = csv.reader(csvfile,delimiter=',')
     times = []
     distance = []
     pret = []
     for row in readCSV:
-        pret.append(row[0])
-        times.append(row[1])
-        distance.append(row[2])
+        pret.append(float(row[0]))
+        times.append(float(row[1]))
+        distance.append(float(row[2]))
 
 
 # Calculate least square
-#ans = leastsq.least_sq(xdata, ydata)
-#print ans
+ans = leastsq.least_sq(distance, times)
+print ans
 
 # Plot graph of the result and the original data
-#leastsq.plot_curve(xdata, ydata)
+leastsq.plot_curve(distance, times)
 
 # Compute the time it takes to get up to max speed
-#t = leastsq.start_time(xdata, ydata)
-#print 'Start time is: %.5f s'% (t)
+t = leastsq.start_time(xdata, ydata)
+print 'Start time is: %.5f s'% (t)
 
 # Compute the speed of the antenna
-#v = leastsq.speed(xdata, ydata)
-#print 'Speed of the antenna: %.5f deg/s'% (v)
+v = leastsq.speed(xdata, ydata)
+print 'Speed of the antenna: %.5f deg/s'% (v)
