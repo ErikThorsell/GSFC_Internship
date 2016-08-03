64b25483a78c55fc5513af6469be3a3f0a1a6511
Erik Thorsell <erithor@student.chalmers.se>
2016-08-03 09:21:20 -0400

    Added runall.sh
            * runall.sh will look up all azel files and run the program
            * extraxctor.py for all the corresponding logs

--- azel/runall.sh
+++ azel/runall.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-LOGDIR=./logs/*
-SKDDIR=./skds/*
-SRCDIR=./script/*
+LOGDIR=./logs/
+SKDDIR=./skds/
+DATADIR=./data/
+SRC=./script/extraxct.py
 
-for f in $SKDDIR; do
-    SKDFILE="$f"
-    echo "$f"
-    for l in $LOGDIR; do
-        echo "$l"
+rm $DATADIR*
+
+for f in $SKDDIR*; do
+    SKDFILE=$f
+    SKDFILE=$(echo $SKDFILE | cut -d'/' -f 3)
+    tSKDFILE=$(echo $SKDFILE | cut -d'.' -f 1)
+    for l in $LOGDIR*; do
+        tLOG=$(echo $l | cut -d'/' -f 3)
+        if [ $tLOG == $tSKDFILE ]; then
+            LOGFILE=$l
+            SKDFILE=$SKDDIR$SKDFILE
+            python2 $SRC $LOGFILE $SKDFILE
+        fi
     done
 done
