89c668617dbf56d6cb1a7f119ded84a0e07ecc8d
Lina Olandersson <olalina@student.chalmers.se>
2016-08-04 14:54:58 -0400

    Cleaned up some code

--- azel/computelstsq/leastsq.py
+++ azel/computelstsq/leastsq.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 #############################################################
 #                                                           #
-# This module computes least square solutions               #
+# This module computes least square solutions for antenna   #
+# slew rate data.                                           #
 #                                                           #
-# least_sq() computes a least square solution               #
 # plot_curve() plots the solution and its original data     #
 # speed() returns the velocity of the antenna in rad/s      #
 # start_time() returns the startup time fori the antenna    #
@@ -16,23 +16,15 @@
 import matplotlib.pyplot as plt
 import numpy
 
-def least_sq(xdata, ydata):
-    x = numpy.array(xdata)
-    y = numpy.array(ydata)
-
-    # Stack arrays vertically, .T transposes the matrix
-    A = numpy.vstack([x, numpy.ones(len(x))]).T
-    # Return the least square solution to a linear matrix equation
-    k,m = numpy.linalg.lstsq(A, y)[0]
-    v = 1/k * 60
-    return "Least square solution: f(x)= %.5f x + %.5f" % (v,m)
 
 def plot_curve(xdata, ydata, pret, stat, ori):
     x = numpy.array(xdata)
     y = numpy.array(ydata)
     t = numpy.array(pret)
 
+    # Stack arrays vertically, .T transposes the matrix
     A = numpy.vstack([x, numpy.ones(len(x))]).T
+    # Return the least square solution to a linear matrix equation
     k,m = numpy.linalg.lstsq(A, y)[0]
 
     plt.plot(x, y, 'bo', label='Real time', markersize=1)
@@ -65,5 +57,3 @@ def start_time(xdata, ydata):
     k,m = numpy.linalg.lstsq(A, y)[0]
 
     return m
-
-
