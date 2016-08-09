210701630d2667ee6e3b7e4ff7bc691284a16c50
Erik Thorsell <erithor@student.chalmers.se>
2016-08-09 11:42:07 -0400

    Updated runall.sh with some text!

--- azel/runall.sh
+++ azel/runall.sh
@@ -7,10 +7,14 @@ SRC1=./script/extraxct.py
 SRC2=./script/antennaslew.py
 IMGDIR=./img/
 OPT=""
+GRAPH=""
 
+echo "Emptying the folders:" $DATADIR "and" $IMGDIR
 rm $DATADIR*
 rm $IMGDIR*
 
+echo "Finding each skd-file and its corresponding log files."
+echo "Running program" $SRC1
 for f in $SKDDIR*; do
     SKDFILE=$(echo $f | cut -d'/' -f 3)
     tSKDFILE=$(echo $SKDFILE | cut -d'.' -f 1)
@@ -26,6 +30,10 @@ done
 
 if [ $# -gt 0 ]; then
     OPT="$1"
+    GRAPH="and any plots are stored in" $IMGDIR
 fi
 
+echo "Running program" $SRC2 $OPT
 python2 $SRC2 $OPT
+
+echo "Execution completed. The data is stored in" $DATADIR $GRAPH
