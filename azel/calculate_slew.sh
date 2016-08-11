32148c97bff0574140d8050b7414fbe756d3a35e
Erik Thorsell <erithor@student.chalmers.se>
2016-08-11 10:42:48 -0400

    Updated calculate_slew.sh to include more sanity checks.
            * The script now checks if the required programs are present.
              If that is not the case the script tries to generate them.

--- azel/calculate_slew.sh
+++ azel/calculate_slew.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+###############################################################################
+# This script acts as the main program for the antenna slewing time           #
+# calculations. It first checks that the necessary programs and directories   #
+# are present and creates them if necessary. The script then launches a       #
+# sequence of programs that calculates the slewing time for the antennas      #
+# specified in the included sessions.                                         #
+#                                                                             #
+# Args: --graph, enables the graph printing feature in calcSlewRates.py       #
+#                                                                             #
+# Author: Erik Thorsell, Summer Internship 2016                               #
+###############################################################################
 
 LOGDIR=./logs/
 SKDDIR=./skds/
 DATADIR=./data/
 SRC1=./src/extract.py
 SRC2=./src/calcSlewRates.py
+SRC3=getStationSpecs.py
+SRC4=create_extract.py
+SRCANTENNA=./src/antenna.cat
 IMGDIR=./img/
 OPT=""
 GRAPH=""
@@ -13,7 +27,28 @@ CLEANIMG=$(shopt -s nullglob dotglob; echo $IMGDIR*)
 CLEANDATA=$(shopt -s nullglob dotglob; echo $DATADIR*)
 
 echo "Starting execution of ./calculate_slew.sh"
+echo " "
 
+## House keeping!
+echo "Running pre processing and house keeping!"
+echo " "
+
+# Checking that the program has been generated.
+if [ ! -f $SRC1 ]; then
+    echo $SRC1 "is not present. Trying to generate..."
+    if [ ! -f $SRCANTENNA ]; then
+        echo "The file" $SRCANTENNA "is not present. Please fetch your copy from gemini."
+        exit
+    fi
+    cd src/
+    python2 $SRC3
+    python2 $SRC4
+    cd ..
+    echo "Generated" $SRC1
+    echo " "
+fi
+
+# Check that img/ and data/ are present in the folder.
 if [ ! -d $IMGDIR ]; then
     echo "Created folder $IMGDIR"
     mkdir $IMGDIR
@@ -24,6 +59,7 @@ if [ ! -d $DATADIR ]; then
     mkdir $DATADIR
 fi
 
+# Empty img/ and data/ if they contain any data.
 if [ ${#CLEANIMG} -gt 0 ]; then
     echo "Emptying directory:" $IMGDIR
     rm $IMGDIR*
@@ -33,9 +69,12 @@ if [ ${#CLEANDATA} -gt 0 ]; then
     echo "Emptying directory:" $DATADIR
     rm $DATADIR*
 fi
+echo " "
 
 echo "Finding each skd-file and its corresponding log files."
+echo " "
 echo "Running program" $SRC1
+echo " "
 for f in $SKDDIR*; do
     SKDFILE=$(echo $f | cut -d'/' -f 3)
     tSKDFILE=$(echo $SKDFILE | cut -d'.' -f 1)
@@ -54,9 +93,12 @@ if [ $# -gt 0 ]; then
     OPT="$1"
 fi
 
+echo " "
 echo "Running program" $SRC2 $OPT
+echo " "
 python2 $SRC2 $OPT
 
 echo "Execution completed."
+echo " "
 echo "The data is stored in" $DATADIR "and any plots are stored in" $IMGDIR
 
