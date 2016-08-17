29ac949d0c64d4aac77d70dd5d7520586ff7c1c1
Erik Thorsell <erithor@student.chalmers.se>
2016-08-17 15:04:18 -0400

    Added trakl/flagr check in azelslew_calc.

--- azel/src/azelslew_calc.py
+++ azel/src/azelslew_calc.py
@@ -66,6 +66,7 @@ def getLogData(path_to_logs, tups):
         for file in files:
             if file[-4:] == ".log":
                 sourcefound = False
+                acquirefound = False
                 nstations = nstations + 1
                 station = file[-6:-4].lower()
                 log = open(os.path.join(subdir, file), 'r')
@@ -84,10 +85,16 @@ def getLogData(path_to_logs, tups):
                         sourcefound = True
                         sourcedate = parseLogTime(line[:20])
                     if ((("#trakl#Source acquired" in line) or \
+                        ("#trakl# Source acquired" in line) or \
                         ('#flagr#flagr/antenna,acquired' in line)) and sourcefound):
+                        acquirefound = True
                         trakldate = parseLogTime(line[:20])
                         if (trakldate > sourcedate):
                             tups.append((station, sourcedate, source, trakldate, direction, file))
+            if not acquirefound:
+                print "# WARNING #"
+                print "Trakl or Flagr is not turned on for station " + station \
+                    + " in session " + file[:-6] + '\n'
 
         return nstations
 
