58efa61a44b5e58b2df554952a95677d961d31a3
Erik Thorsell <erithor@student.chalmers.se>
2016-08-17 15:31:27 -0400

    Started working on new calculate_slew.sh.
            * The old one can be found under calculate_slew.sh.old

--- azel/calculate_slew.sh
+++ azel/calculate_slew.sh
@@ -11,102 +11,76 @@
 # Author: Erik Thorsell, Summer Internship 2016                               #
 ###############################################################################
 
-LOGDIR=./logs/
-SKDDIR=./skds/
-DATADIR=./data/
 SRC1=./src/extract.py
 SRC2=./src/calcSlewRates.py
 SRC3=getStationSpecs.py
 SRC4=create_extract.py
-SRCANTENNA=./src/antenna.cat
-IMGDIR=./img/
+SRCANTENNA=/shared/gemini/ftp/pub/sked/catalogs/antenna.cat
+OUTPUTDIR=""
+DATADIR=""
+IMGDIR=""
+SESSIONPATH=""
 OPT=""
 GRAPH=""
-
-CLEANIMG=$(shopt -s nullglob dotglob; echo $IMGDIR*)
-CLEANDATA=$(shopt -s nullglob dotglob; echo $DATADIR*)
-
-echo "Starting execution of ./calculate_slew.sh"
-echo " "
-
-## House keeping!
-echo "Running pre processing and house keeping!"
-echo " "
-
-# Checking that the program has been generated.
-if [ ! -f $SRC1 ]; then
-    echo $SRC1 "is not present. Trying to generate..."
-    if [ ! -f $SRCANTENNA ]; then
-        echo "The file" $SRCANTENNA "is not present. Please fetch your copy from gemini."
-        exit
-    fi
-    cd src/
-    python2 $SRC3
-    python2 $SRC4
-    cd ..
-    echo "Generated" $SRC1
-    echo " "
-fi
-
-# Check that img/ and data/ are present in the folder.
-if [ ! -d $IMGDIR ]; then
-    echo "Created folder $IMGDIR"
-    mkdir $IMGDIR
-fi
-
-if [ ! -d $DATADIR ]; then
-    echo "Created folder $DATADIR"
-    mkdir $DATADIR
-fi
-
-# Empty img/ and data/ if they contain any data.
-if [ ${#CLEANIMG} -gt 0 ]; then
-    echo "Emptying directory:" $IMGDIR
-    rm $IMGDIR*
-fi
-
-if [ ${#CLEANDATA} -gt 0 ]; then
-    echo "Emptying directory:" $DATADIR
-    rm $DATADIR*
-fi
-echo " "
-
-echo "Finding each skd-file and its corresponding log files."
-echo " "
-if [ ! -d $SKDDIR ]; then
-    echo "Unable to locate" $SKDDIR "please read the README.md for more info on how to use the program."
-    exit
-fi
-echo "Running program" $SRC1
-echo " "
-for f in $SKDDIR*; do
-    SKDFILE=$(echo $f | cut -d'/' -f 3)
-    tSKDFILE=$(echo $SKDFILE | cut -d'.' -f 1)
-    if [ ! -d $LOGDIR ]; then
-        echo "Unable to locate" $LOGDIR "please read the README.md for more info on how to use the program."
-        exit
-    fi
-    echo "Working on session" $tSKDFILE
-    for l in $LOGDIR*; do
-        tLOG=$(echo $l | cut -d'/' -f 3)
-        if [ $tLOG == $tSKDFILE ]; then
-            LOGFILES=$l
-            SKDFILE=$SKDDIR$SKDFILE
-            python2 $SRC1 $LOGFILES $SKDFILE
-        fi
-    done
-done
+var=""
 
 if [ $# -gt 0 ]; then
-    OPT="$1"
+    echo "# ERROR #"
+    echo "Invalid usage. The program will prompt you for the information
+          required."
+    exit
 fi
 
-echo " "
-echo "Running program" $SRC2 $OPT
-echo " "
-python2 $SRC2 $OPT
+echo "Running calculate_slew.sh"
+echo "Please provide the path(s) to the sessions you wish to analyze:"
+IFS=' ' read -ra SESSIONPATH
 
-echo "Execution completed."
-echo " "
-echo "The data is stored in" $DATADIR "and any plots are stored in" $IMGDIR
+echo "Please provide the path to where you want the output to be stored:"
+read OUTPUTDIR
+DATADIR=$OUTPUTDIR/data/
+IMGDIR=$OUTPUTDIR/img/
 
+echo $DATADIR
+echo $IMGDIR
+
+for SESSIONDIR in "${SESSIONPATH[@]}"; do
+    echo $SESSIONDIR
+done
+
+#if [ ! -d $SKDDIR ]; then
+#    echo "Unable to locate" $SKDDIR "please read the README.md for more info on how to use the program."
+#    exit
+#fi
+#echo "Running program" $SRC1
+#echo " "
+#for f in $SKDDIR*; do
+#    SKDFILE=$(echo $f | cut -d'/' -f 3)
+#    tSKDFILE=$(echo $SKDFILE | cut -d'.' -f 1)
+#    if [ ! -d $LOGDIR ]; then
+#        echo "Unable to locate" $LOGDIR "please read the README.md for more info on how to use the program."
+#        exit
+#    fi
+#    echo "Working on session" $tSKDFILE
+#    for l in $LOGDIR*; do
+#        tLOG=$(echo $l | cut -d'/' -f 3)
+#        if [ $tLOG == $tSKDFILE ]; then
+#            LOGFILES=$l
+#            SKDFILE=$SKDDIR$SKDFILE
+#            python2 $SRC1 $LOGFILES $SKDFILE
+#        fi
+#    done
+#done
+#
+#if [ $# -gt 0 ]; then
+#    OPT="$1"
+#fi
+#
+#echo " "
+#echo "Running program" $SRC2 $OPT
+#echo " "
+#python2 $SRC2 $OPT
+#
+#echo "Execution completed."
+#echo " "
+#echo "The data is stored in" $DATADIR "and any plots are stored in" $IMGDIR
+#
